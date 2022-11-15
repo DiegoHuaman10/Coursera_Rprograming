482ec467f5154ccb3b783a416e2bd7d7ced43f7e
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-11-14 20:25:13 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datetime import time
-
-import streamlit as st
-import pandas as pd
-import numpy as np
-from datetime import time
+import urllib.request
 
 st.title("Licenciamiento Institucional")
 st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional de las Universidades peruanas. Incluye información de región y tipo de entidad.")
 st.text("[Dar un contexto---alguién puede ir escribiendo esto bien referenciado]")
+
+@st.experimental_memo
+def download_data():
+  url = "https://www.datosabiertos.gob.pe/sites/default/files/Licenciamiento%20Institucional_7.csv"
+  filename = 'Licenciamiento Institucional_7.csv'
+  urllib.request.urlretrieve(url, filename)
+download_data()
