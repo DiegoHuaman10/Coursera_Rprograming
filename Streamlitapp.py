2af8b466c82db6bd7ed50ed9c4e621e4ce45eaa3
DiegoHuaman10 <117952972+DiegoHuaman10@users.noreply.github.com>
2022-11-14 20:10:18 -0500

    Delete Streamlitapp.py

--- Streamlitapp.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-from datetime import time
-
-st.title("Licenciamiento Institucional")
-st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional de las Universidades peruanas. Incluye información de región y tipo de entidad.")
-st.text("[Dar un contexto---alguién puede ir escribiendo esto bien referenciado]")
