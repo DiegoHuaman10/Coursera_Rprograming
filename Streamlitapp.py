002756682f68f432da56c401d192396851ec854d
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-11-14 19:50:48 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -3,13 +3,4 @@ import pandas as pd
 import numpy as np
 from datetime import time
 
-#appointment = st.slider(
-#"Programe la asesoria:",
-#value=(time(11, 30), time(12, 45)))
-#st.write("Esta agendado para:", appointment)
-
-st.title("Gráfico")
-n = st.slider("n", 5,100, step=1)
-chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
-st.line_chart(chart_data)
-
+st.title("Licenciamiento Institucional")
