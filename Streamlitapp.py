c5d96e0ed6eb6900424f03a78b5b9c408a333d33
DiegoHuaman10 <117952972+DiegoHuaman10@users.noreply.github.com>
2022-11-14 20:35:49 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -7,9 +7,11 @@ st.title("Licenciamiento Institucional")
 st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional de las Universidades peruanas. Incluye información de región y tipo de entidad.")
 st.text("[Dar un contexto---alguién puede ir escribiendo esto bien referenciado]")
 
-@st.experimental_memo
-def download_data():
-  url = "https://www.datosabiertos.gob.pe/sites/default/files/Licenciamiento%20Institucional_7.csv"
-  filename = 'Licenciamiento Institucional_7.csv'
-  urllib.request.urlretrieve(url, filename)
-download_data()
+#@st.experimental_memo
+#def download_data():
+ # url = "https://www.datosabiertos.gob.pe/sites/default/files/Licenciamiento%20Institucional_7.csv"
+ # filename = 'Licenciamiento Institucional_7.csv'
+ # urllib.request.urlretrieve(url, filename)
+#download_data()
+a = pd.read_csv('Licenciamiento Institucional_7.csv')
+print(a)
