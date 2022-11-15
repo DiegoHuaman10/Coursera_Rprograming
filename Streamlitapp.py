0dcc5b7d53a6453050485b8eb785c72fbf7d358e
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-11-14 20:08:34 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -5,4 +5,4 @@ from datetime import time
 
 st.title("Licenciamiento Institucional")
 st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional de las Universidades peruanas. Incluye información de región y tipo de entidad.")
-st.text("[Dar un contexto---alguién puede ir escribiendo esto]")
+st.text("[Dar un contexto---alguién puede ir escribiendo esto bien referenciado]")
