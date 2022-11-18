f54e2ffe50db114ac845a20850cec717955c7a55
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-11-18 07:33:24 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -6,12 +6,7 @@ import urllib.request
 st.title("Licenciamiento Institucional")
 st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional de las Universidades peruanas. Incluye información de región y tipo de entidad.")
 st.text(
-        "Mostrar al público qué instituciones del país son licenciadas por la, 
-        Superintendencia Nacional de Educación Superior (SUNEDU) es importante, 
-        ya que permite conocer cuáles cumplen con las condiciones básicas de calidad, 
-        como una buena infraestructura, además, garantiza la calidad académica y, 
-        eficiencia de la formación de un futuro profesional como la proyección, 
-        laboral en beneficio del estudiante."
+        "Mostrar al público qué instituciones del país son licenciadas por la Superintendencia Nacional de Educación Superior (SUNEDU) es importante, ya que permite conocer cuáles cumplen con las condiciones básicas de calidad, como una buena infraestructura, además, garantiza la calidad académica y eficiencia de la formación de un futuro profesional como la proyección laboral en beneficio del estudiante."
 )
 
 #@st.experimental_memo
