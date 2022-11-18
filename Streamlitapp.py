8b48b3771f90586c758f21f8008cfcd9ab953ecb
DiegoHuaman10 <117952972+DiegoHuaman10@users.noreply.github.com>
2022-11-18 08:29:51 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -8,12 +8,12 @@ st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional d
 st.text("Mostrar al público qué instituciones del país son licenciadas por la Superintendencia Nacional de Educación Superior (SUNEDU) es importante, ya que permite conocer cuáles cumplen con las condiciones básicas de calidad, la infraestructura, además, garantiza la calidad académica y eficiencia de la formación de un futuri profesional como la proyección laboral en un beneficio del estudiante")
 
 
-url = "https://raw.githubusercontent.com/DiegoHuaman10/Proyecto-Prograavanzada/main/LicenciamientoInstitucional_7.csv"
+url = "https://raw.githubusercontent.com/DiegoHuaman10/Proyecto-Prograavanzada/main/Licenciamiento%20Institucional_7.csv"
 # filename = 'Licenciamiento Institucional_7.csv'
 # urllib.request.urlretrieve(url, filename)
 #download_data()
 
-file = pd.read_csv(url, sep= ';')
+file = pd.read_csv(url, sep= '|')
 
 print(file)
 
