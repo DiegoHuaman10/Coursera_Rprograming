680815f4d778d9f427621b09506ef17f63c6d237
Verenice Sobrado <117706174+nayeli-verenice@users.noreply.github.com>
2022-11-21 19:30:03 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 st.title("Licenciamiento Institucional")
-st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional de las Universidades peruanas. Incluye información de región y tipo de entidad.")
+st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional de las universidades peruanas. Incluye información de región y tipo de entidad.")
 st.write("Mostrar al público qué instituciones del país son licenciadas por la Superintendencia Nacional de Educación Superior (SUNEDU) es importante, ya que permite conocer cuáles cumplen con las condiciones básicas de calidad, la infraestructura, además,  garantiza la calidad académica y eficiencia en la formación del futuro profesional como la proyección laboral en beneficio del estudiante")
 
 
