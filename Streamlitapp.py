4eea2d537fabc76bee7b151a926ddecc8fc7487d
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-11-23 01:07:48 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.sidebar.title("¿Quiénes somos?")
-st.write("Somos un grupo de estudiantes de V ciclo de Ingeniería de la Universidad Peruano Cayetano Heredia (UPCH)")
+st.sidebar.write("Somos un grupo de estudiantes de V ciclo de Ingeniería de la Universidad Peruano Cayetano Heredia (UPCH)")
 
 st.title("Licenciamiento Institucional")
 st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional de las Universidades peruanas. Incluye información de región y tipo de entidad.")
