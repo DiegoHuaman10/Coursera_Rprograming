95000b3d881a8fdefc8c88baa63fc98588bc1219
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-11-25 08:11:46 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -18,6 +18,16 @@ st.title("Licenciamiento Institucional")
 st.subheader("Se presenta el avance y estatus del Licenciamiento Institucional de las Universidades peruanas. Incluye información de región y tipo de entidad.")
 st.write("Mostrar al público qué instituciones del país son licenciadas por la Superintendencia Nacional de Educación Superior (SUNEDU) es importante, ya que permite conocer cuáles cumplen con las condiciones básicas de calidad, la infraestructura, además,  garantiza la calidad académica y eficiencia en la formación del futuro profesional como la proyección laboral en beneficio del estudiante")
 
+#Ubicación de universidades por regiones
+st.subheader("Universidades en el Perú")
+st.write("[Agregar texto]")
+url4='https://raw.githubusercontent.com/DiegoHuaman10/Proyecto-Prograavanzada/main/Ubicaci%C3%B3n.csv'
+file4 = pd.read_csv(url4, sep= ',')
+st.map(file4)
+
+
+
+
 st.write("¿Cómo desea revisar la información?")
 st.selectbox("Por ubicación",("AMAZONAS","ÁNCASH","APURIMAC","AREQUIPA","AYACUCHO","CAJAMARCA","CALLAO","CUSCO","HUANCAVELICA","HUANUCO","ICA","JUNIN","LA LIBERTAD","LAMBAYEQUE","LIMA","LORETO","MADRE DE DIOS","MOQUEGUA","PASCO","PIURA","PUNO","SAN MARTIN","TACNA","TUMBES","UCAYALI"))
 st.selectbox("Por el tipo de gestión",("PRIVADAS","NACIONALES"))
@@ -47,12 +57,6 @@ url3='https://raw.githubusercontent.com/DiegoHuaman10/Proyecto-Prograavanzada/ma
 file3 = pd.read_csv(url3, sep= ',')
 st.line_chart(data=file3, x='NOMBRE', y='ESTADO_LICENCIAMIENTO')
 
-#Ubicación de universidades por regiones
-st.subheader("Ubicación de universidades por regiones")
-st.write("[Agregar texto]")
-url4='https://raw.githubusercontent.com/DiegoHuaman10/Proyecto-Prograavanzada/main/Ubicaci%C3%B3n.csv'
-file4 = pd.read_csv(url4, sep= ',')
-st.map(file4)
 
 tab1, tab2 = st.tabs(["Universidades Privadas", "Universidades Nacionales"])
 tab1.write("Aquí poner un seleccionador")
