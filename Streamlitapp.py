703ae00deac2c8c7934b0801158a883913887f95
Solait17 <117995980+Solait17@users.noreply.github.com>
2022-11-25 09:20:32 -0500

    Create Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -4,14 +4,14 @@ import numpy as np
 
 st.sidebar.title("Programación Avanzada")
 if st.sidebar.button("¿Quiénes somos?"):
-    st.sidebar.write("Somos un grupo de estudiantes de V ciclo de Ingeniería de la Universidad Peruano Cayetano Heredia (UPCH)...")
-    st.sidebar.subheader("Diego Huamán...")
-    st.sidebar.subheader("Solait Alejandra...")
-    st.sidebar.subheader("Verenice Sobrado...")
-    st.sidebar.subheader("Eyvind Herrera...")
+    st.sidebar.write("Somos un grupo de estudiantes de V ciclo de Ingeniería de la Universidad Peruano Cayetano Heredia (UPCH) que ha elaborado como proyecto final del curso de “Programación Avanzada”  una página web con el tema “SUNEDU - Licenciamiento Institucional ”")
+    st.sidebar.subheader("Diego  Manuel Huamán Abad")
+    st.sidebar.subheader("Solait Alejandra De La Cruz Reyes")
+    st.sidebar.subheader(" Neyeli Verenice Sobrado Vergara")
+    st.sidebar.subheader("Eyvind Francisco Herrera More")
 
 if st.sidebar.button("Objetivos"):
-    st.sidebar.write("Buscamos crear una página web con la Dataset de SUNEDU - Licenciamiento Institucional que sea interactiva y de fácil comprensión para nuestro usuario poniendo en práctica todo lo aprendido en el curso de Programación Avanzada .")
+    st.sidebar.write("Buscamos crear una página interactiva y de fácil comprensión para nuestro usuario  con la Dataset de SUNEDU - Licenciamiento Institucional poniendo en práctica todo lo aprendido en el curso de Programación Avanzada.")
 
 
 st.title("Licenciamiento Institucional")
@@ -28,19 +28,13 @@ st.write("**¿Cómo desea revisar la información?**")
 st.selectbox("Por ubicación",("AMAZONAS","ÁNCASH","APURIMAC","AREQUIPA","AYACUCHO","CAJAMARCA","CALLAO","CUSCO","HUANCAVELICA","HUANUCO","ICA","JUNIN","LA LIBERTAD","LAMBAYEQUE","LIMA","LORETO","MADRE DE DIOS","MOQUEGUA","PASCO","PIURA","PUNO","SAN MARTIN","TACNA","TUMBES","UCAYALI"))
 st.selectbox("Por el tipo de gestión",("PRIVADAS","NACIONALES"))
 
-@st.experimental_memo
-def download_data():
-    url="https://raw.githubusercontent.com/DiegoHuaman10/Proyecto-Prograavanzada/main/LicenciamientoInstitucional_7_2.csv"
-    filename="LicenciamientoInstitucional_7_2.csv"
-    df=pd.read_csv("LicenciamientoInstitucional_7_2.csv")
-    return df
-data=download_data()
-st.write("**Tabla de datos generales**")
-st.dataframe(data)
-st.write("**Características del Dataset**")
-st.write(data.describe())
+url="https://raw.githubusercontent.com/DiegoHuaman10/Proyecto-Prograavanzada/main/LicenciamientoInstitucional_7_2.csv"
+filename="LicenciamientoInstitucional_7_2.csv"
+df=pd.read_csv("LicenciamientoInstitucional_7_2.csv")
+print df
+st.subheader("**Características del Dataset**")
+st.write(df.describe())
            
-    
     
     
     
