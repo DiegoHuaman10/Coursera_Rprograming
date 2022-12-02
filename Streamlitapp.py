6cc0e6837c2fb825d32004565768f10769280995
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-12-02 09:48:33 -0500

    Create Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -49,12 +49,13 @@ tab1, tab2, tab3, tab4 = st.tabs(["**Periodo de licenciamiento**", "**Tipo de ge
 with tab1:
    st.write("El periodo de licenciamiento refiere al tiempo por el cual la universidad ha recibido el licenciamiento. El tiempo mínimo de licenciamiento es de 6 años, además, tambien hay periodos de 8 y 10 años. Se otorga la mayor cantidad de años a las universidades que impulsan proyectos de investigación, apoyan a sus docentes investigadores y buscan que un mayor número de estudiantes escriban artículos que puedan ser publicados en alguna revista.")
    url ="https://raw.githubusercontent.com/DiegoHuaman10/Proyecto-Prograavanzada/main/LicenciamientoInstitucional.csv"
-   file = pd.read_csv(url, sep= ',')
-   file[file["PERIODO_LICENCIAMIENTO"]].nunique()
-   
+   df_pl= df.PERIODO_LICENCIAMIENTO.value_counts()
+   st.bar_chart(df_pl)
+
 with tab2:
-   st.write("Actualmente existen ....")
-   df[df["TIPO_GESTION"]].nunique()
+   df_gestion= df.TIPO_GESTION.value_counts()
+   st.write('Distribución por el tipo de gestión:')
+   st.bar_chart(df_gestion)
 
 with tab3:
    st.write("En la actualidad, en cada región del Perú, existe al menos una a más universidades públicas o privadas. Lo cual significa, que cada habitante tiene mayor acceso a la educación, así como también la oportunidad de estudiar más cerca a sus hogares.")
