0864bdda85b6d468ad5aeacccf6a42346cf1a2d6
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-12-02 08:33:35 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -59,9 +59,8 @@ with tab2:
 with tab3:
    st.write("En la actualidad, en cada región del Perú, existe al menos una a más universidades públicas o privadas. Lo cual significa, que cada habitante tiene mayor acceso a la educación, así como también la oportunidad de estudiar más cerca a sus hogares.")
    text_imput=st.text_input("**Ingrese la región para conocer qué universidades se encuentran en el lugar indicado👇 (Escribir en MAYÚSCULAS)**",)
-   new_df=df[df["DEPARTAMENTO"]==text_imput]
-   df(new_df)
-
+   df[df["DEPARTAMENTO"]==text_imput]
+   
 with tab4:
    text_imput=st.text_input("**Ingrese las SIGLAS del nombre de la universidad de su interés 👇 (Escribir en MAYÚSCULAS)**",)
    df[df["SIGLAS"]==text_imput]
