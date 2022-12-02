069c71d5cc59b07c57e7ccc11f8ae56e9c01e3b7
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-12-02 09:19:55 -0500

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -55,7 +55,7 @@ with tab1:
 
 with tab2:
    st.write("Actualmente existen ....")
-   df["TIPO_GESTION"].nunique()
+   df[df["TIPO_GESTION"]].nunique()
 
 with tab3:
    st.write("En la actualidad, en cada región del Perú, existe al menos una a más universidades públicas o privadas. Lo cual significa, que cada habitante tiene mayor acceso a la educación, así como también la oportunidad de estudiar más cerca a sus hogares.")
