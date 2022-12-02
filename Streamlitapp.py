b3c04eee74dfc2d8a01551c4b3bfb0879755bbfa
EyvindH <117953052+EyvindH@users.noreply.github.com>
2022-12-02 08:31:42 -0500

    Create Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -51,7 +51,7 @@ with tab1:
    url ="https://raw.githubusercontent.com/DiegoHuaman10/Proyecto-Prograavanzada/main/LicenciamientoInstitucional.csv"
    file = pd.read_csv(url, sep= ',')
    st.line_chart(data=file, x='NOMBRE', y='PERIODO_LICENCIAMIENTO')
-   st.bar_chart(file, x='SIGLAS', y='PERIODO_LICENCIAMIENTO')
+   st.bar_chart(file, x='NOMBRE', y='PERIODO_LICENCIAMIENTO')
 
 with tab2:
    st.write("Actualmente existen ....")
@@ -60,7 +60,7 @@ with tab3:
    st.write("En la actualidad, en cada región del Perú, existe al menos una a más universidades públicas o privadas. Lo cual significa, que cada habitante tiene mayor acceso a la educación, así como también la oportunidad de estudiar más cerca a sus hogares.")
    text_imput=st.text_input("**Ingrese la región para conocer qué universidades se encuentran en el lugar indicado👇 (Escribir en MAYÚSCULAS)**",)
    new_df=df[df["DEPARTAMENTO"]==text_imput]
-   print(new_df)
+   df.new_df
 
 with tab4:
    text_imput=st.text_input("**Ingrese las SIGLAS del nombre de la universidad de su interés 👇 (Escribir en MAYÚSCULAS)**",)
